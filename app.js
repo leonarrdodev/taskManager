51fd59d7a53128e6447e684054eed30f92e04b6d
Leonardo <leonardo.pro.7004@gmail.com>
2025-07-24 00:08:53 -0300

    att

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('node:path')
+const taskRoutes = require('./routes/taskRoutes')
+
 
 const app = express()
 
@@ -7,5 +9,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use('/tasks', taskRoutes)
 
 module.exports = app
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,4 +2,10 @@ const express = require('express')
 const router = express.Router()   
 const tasksController = require('../controllers/taskController')
 
+
 router.get('/', tasksController.index)
+router.post('/', tasksController.create)
+router.post('/:id/toggle', tasksController.toggleStatus)
+router.post('/:id/delete', tasksController.remove)
+
+module.exports = router
